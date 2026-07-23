be4ce7fed11c3f59de12ea5a47daee90fc8c42bb
LucasGottero <lucasgottero4@gmail.com>
2026-07-23 09:32:21 -0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,13 @@
+# GWS Weather Station
+
+Dashboard IoT desarrollado por GWS Solutions.
+
+Características:
+
+- Lectura automática de ThingSpeak
+- Temperatura °C
+- Temperatura °F
+- Humedad
+- Alarma
+- Gráfico en tiempo real
+- Actualización cada 15 segundos
---
+++ script.js
@@ -0,0 +1,97 @@
+const CHANNEL = "2879152";
+
+const API = "https://api.thingspeak.com/channels/"+CHANNEL+"/feeds.json?api_key=M1POOBTT61ND0UTV&results=20";
+
+let chart;
+
+async function cargar(){
+
+const respuesta = await fetch(API);
+
+const datos = await respuesta.json();
+
+const ultimo = datos.feeds[datos.feeds.length-1];
+
+document.getElementById("tempC").innerHTML=ultimo.field1+" °C";
+
+document.getElementById("tempF").innerHTML=ultimo.field2+" °F";
+
+document.getElementById("humedad").innerHTML=ultimo.field3+" %";
+
+document.getElementById("fecha").innerHTML=new Date(ultimo.created_at).toLocaleString();
+
+const alarma=document.getElementById("alarma");
+
+const estado=document.getElementById("estadoAlarma");
+
+if(ultimo.field4=="1"){
+
+estado.innerHTML="🚨 ACTIVADA";
+
+alarma.style.background="#ff3b30";
+
+alarma.style.color="white";
+
+}else{
+
+estado.innerHTML="🟢 NORMAL";
+
+alarma.style.background="#4CAF50";
+
+alarma.style.color="white";
+
+}
+
+const etiquetas=[];
+
+const temperaturas=[];
+
+datos.feeds.forEach(f=>{
+
+etiquetas.push(f.created_at.substring(11,16));
+
+temperaturas.push(f.field1);
+
+});
+
+if(chart){
+
+chart.destroy();
+
+}
+
+chart=new Chart(document.getElementById("chartTemp"),{
+
+type:"line",
+
+data:{
+
+labels:etiquetas,
+
+datasets:[{
+
+label:"Temperatura °C",
+
+data:temperaturas,
+
+fill:false,
+
+borderWidth:3
+
+}]
+
+},
+
+options:{
+
+responsive:true
+
+}
+
+});
+
+}
+
+cargar();
+
+setInterval(cargar,15000);
